938e9f161d0fc358c3d253d0ece42bd18a08322e
Muhammad-1971 <shohanurislam1971@gmail.com>
2021-10-03 19:30:40 -0800

    update

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+  return (
+    <div>
+      <h2>This  is inventory </h2>
+      <h4>I am back</h4>
+    </div>
+  );
+};
+
+export default Inventory;
